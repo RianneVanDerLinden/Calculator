60549ae79eeb0e3a75c6d53a2e1a2c66d0694000
RianneVanDerLinden <47359535+RianneVanDerLinden@users.noreply.github.com>
2019-02-11 11:24:28 +0100

    Update Calculator
    Looks like a forgot the parenthesis on my added function

--- Calcuator/Program.cs
+++ Calcuator/Program.cs
@@ -12,7 +12,7 @@ namespace Calcuator
         {
             //Comment
             Console.WriteLine(1 + 2);
-            Console.WriteLine a - b ;
+            Console.WriteLine (a - b) ;
             Console.ReadKey();
             
             //Propose Pull Request
